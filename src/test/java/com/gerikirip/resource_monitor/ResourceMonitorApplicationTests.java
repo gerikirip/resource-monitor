864274cc68001448120bd3b88ed8667f8df0c9cc
Kirjak Gergely <gerimobb@gmail.com>
2026-05-10 13:20:27 +0200

    ci: run tests with test profile

--- src/test/java/com/gerikirip/resource_monitor/ResourceMonitorApplicationTests.java
+++ src/test/java/com/gerikirip/resource_monitor/ResourceMonitorApplicationTests.java
@@ -2,8 +2,10 @@ package com.gerikirip.resource_monitor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ResourceMonitorApplicationTests {
 
 	@Test
