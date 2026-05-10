9c4978a5868bf42c381760b0e53a598d4242af6f
Kirjak Gergely <gerimobb@gmail.com>
2026-05-10 11:24:37 +0200

    Initial commit - Spring Initializr

---
+++ src/main/java/com/gerikirip/resource_monitor/ResourceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.gerikirip.resource_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerikirip/resource_monitor/ResourceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerikirip.resource_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
